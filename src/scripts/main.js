bb05f310d6e5d51f3e589c137e3aac5b8403afc9
aagarw30 <tuanle0984946840@gmail.com>
2018-08-22 19:41:16 +0700

    add

---
+++ src/scripts/main.js
@@ -0,0 +1,22 @@
+// Main
+console.log("aaa")
+$(document).ready(function() {
+    $('.home-partner .owl-carousel').owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+
+});
